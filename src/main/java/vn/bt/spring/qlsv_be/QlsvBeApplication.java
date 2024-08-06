05a06db0a7e268f996b8399510b243e9dcafe794
Le Quoc Viet <lequocviet2752004@gmail.com>
2024-08-06 07:09:16 +0700

    init project

---
+++ src/main/java/vn/bt/spring/qlsv_be/QlsvBeApplication.java
@@ -0,0 +1,13 @@
+package vn.bt.spring.qlsv_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlsvBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlsvBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/bt/spring/qlsv_be/QlsvBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.bt.spring.qlsv_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlsvBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
